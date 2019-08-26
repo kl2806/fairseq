d81ce0a27a58740e5c461de8472e7169e40e4762
Naman Goyal <namangoyal@devfair0110.h2.fair>
2019-08-26 13:13:21 -0700

    fix for packaging numpy headers for cython

--- setup.py
+++ setup.py
@@ -27,8 +27,40 @@ bleu = Extension(
     extra_compile_args=extra_compile_args,
 )
 
-token_block_utils = [Extension("fairseq.data.token_block_utils_fast", ["fairseq/data/token_block_utils_fast.pyx"])]
-data_utils_fast = [Extension("fairseq.data.data_utils_fast", ["fairseq/data/data_utils_fast.pyx"], language="c++")]
+
+def get_cython_modules():
+    token_block_utils = Extension(
+        "fairseq.data.token_block_utils_fast",
+        ["fairseq/data/token_block_utils_fast.pyx"],
+    )
+    data_utils_fast = Extension(
+        "fairseq.data.data_utils_fast",
+        ["fairseq/data/data_utils_fast.pyx"],
+        language="c++",
+    )
+    return [token_block_utils, data_utils_fast]
+
+
+def my_build_ext(pars):
+    """
+    Hack to import numpy headers for cython without assuming numpy is
+    pre-installed on the system.
+    More details: https://stackoverflow.com/questions/54117786/add-numpy-get-include-argument-to-setuptools-without-preinstalled-numpy
+    """
+    # import delayed:
+    from setuptools.command.build_ext import build_ext as _build_ext
+
+    # include_dirs adjusted:
+    class build_ext(_build_ext):
+        def finalize_options(self):
+            _build_ext.finalize_options(self)
+            # Prevent numpy from thinking it is still in its setup process:
+            __builtins__.__NUMPY_SETUP__ = False
+            import numpy
+            self.include_dirs.append(numpy.get_include())
+    # Object returned:
+    return build_ext(pars)
+
 
 setup(
     name='fairseq',
@@ -45,6 +77,7 @@ setup(
     long_description=readme,
     long_description_content_type='text/markdown',
     setup_requires=[
+        'numpy',
         'cython',
         'setuptools>=18.0',
     ],
@@ -58,7 +91,7 @@ setup(
         'tqdm',
     ],
     packages=find_packages(exclude=['scripts', 'tests']),
-    ext_modules=token_block_utils + data_utils_fast + [bleu],
+    ext_modules=get_cython_modules() + [bleu],
     test_suite='tests',
     entry_points={
         'console_scripts': [
@@ -71,5 +104,6 @@ setup(
             'fairseq-validate = fairseq_cli.validate:cli_main',
         ],
     },
+    cmdclass={'build_ext': my_build_ext},
     zip_safe=False,
 )
